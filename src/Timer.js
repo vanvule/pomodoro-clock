4fb7b3f04786a71176aba2c2926e202e04b6c921
Le Van Vu <vule@teqnological.asia>
2022-04-01 14:10:53 +0700

    update responsive

--- src/Timer.js
+++ src/Timer.js
@@ -8,21 +8,23 @@ import { useClockify } from './hooks/useClockify';
 
 const Timer = () => {
 
-  const [{projectName, timerLabel}] = useStateValue();
+  const [{ projectName, timerLabel }] = useStateValue();
   const clockifiedValue = useClockify();
 
   return (
-    <div className="timer">
-      <h2 className='timer__maintitle'>{projectName}</h2>
-      <h3 id='timer-label' className='timer__label'>{timerLabel}</h3>
-      <h1 id='time-left' className='timer__time'>{clockifiedValue}</h1>
-      <Controls />
-      <div className="timer__controllers">
-        <Break />
-        <Session />
-      </div>
+    <>
       <ThemeToggle />
-    </div>
+      <div className="timer">
+        <h2 className='timer__maintitle'>{projectName}</h2>
+        <h3 id='timer-label' className='timer__label'>{timerLabel}</h3>
+        <h1 id='time-left' className='timer__time'>{clockifiedValue}</h1>
+        <Controls />
+        <div className="timer__controllers">
+          <Break />
+          <Session />
+        </div>
+      </div>
+    </>
   );
 }
 
